92b45c8aa81d91fcf22b809d182c21113a3cb204
Rufino <guilhermerspg@gmail.com>
2024-03-26 17:29:01 -0300

    f strings

--- aula13.py
+++ aula13.py
@@ -1,10 +1,10 @@
-nome = 'Luiz Otávio'
-altura = 1.80
-peso = 95
-imc = peso / altura ** 2
+nome = 'Guilherme Rufino'
+altura = 1.83
+peso = 60
+imc = peso / (altura * altura)
 
 "f-strings"
-linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_1 = f'{nome} tem {altura:,.2f} de altura,'
 linha_2 = f'pesa {peso} quilos e seu imc é'
 linha_3 = f'{imc:.2f}'
 
