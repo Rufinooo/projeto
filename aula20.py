5f246b987457eb505144e037677cf8c2a18e121c
Rufino <110191735+Rufinooo@users.noreply.github.com>
2024-04-20 20:18:27 -0300

    Create aula20.py

---
+++ aula20.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'O primeiro valor que é {primeiro_valor} é maior do que o segundo valor que é {segundo_valor}.')
+else:
+    print(f'O segundo valor que é {segundo_valor} é maior do que o primeiro valor que é {primeiro_valor}.')
