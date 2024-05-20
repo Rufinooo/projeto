01dc9ee377d1fa001d4e9299666c0da7063961b0
Rufino <110191735+Rufinooo@users.noreply.github.com>
2024-05-20 17:38:56 -0300

    Update aula32.py

--- aula32.py
+++ aula32.py
@@ -42,15 +42,15 @@ menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
 
-nome = input('digite seu nome: ')
-tam_nome = len(nome)
+nome = input('Digite seu nome: ')
+tamanho_nome = len(nome)
 
-try:
-    if tam_nome <= 4:
-        print('Seu nome é pequeno.')
-    elif tam_nome >= 5 and tam_nome <= 6:
-        print('Seu nome é normal.')
-    elif tam_nome >= 7:
-        print('Seu nome é grande.')
-except:
-    print('Algo de errado não está certo.')
+if tamanho_nome > 1:
+    if tamanho_nome <= 4:
+        print('Seu nome é curto')
+    elif tamanho_nome >= 5 and tamanho_nome <= 6:
+        print('Seu nome é normal')
+    else:
+        print('Seu nome é muito grande')
+else:
+    print('Digite mais de uma letra.')
